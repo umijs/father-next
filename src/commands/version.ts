8228d0ebbb6a0b34bef4754c129af61045d54cec
xiaohuoni <chen.xiaocong2@iwhalecloud.com>
2022-06-27 09:49:44 +0800

    chore: remove try catch

--- src/commands/version.ts
+++ src/commands/version.ts
@@ -10,31 +10,30 @@ export default (api: IApi) => {
       if (!args.quiet) {
         const versions = [`father@${version}`];
         // parts version
-        const parts = [
-          '@vercel/ncc',
-          '@babel/core',
-          'esbuild',
-          'webpack',
-          'typescript',
-          '@microsoft/api-extractor',
-        ];
-
-        parts.forEach((part) => {
+        const parts = ['@vercel/ncc', 'typescript', '@microsoft/api-extractor'];
+        // @umijs/bundler-utils
+        const utilsParts = ['@babel/core', 'esbuild'];
+        // @umijs/bundler-webpack
+        const bundlerParts = ['webpack'];
+        utilsParts.forEach((part) => {
           let version = 'unknown';
-          try {
-            // try require part package
-            version = require(`${part}/package.json`).version;
-          } catch (error) {
-            // try find part version from @umijs/bundler-utils or @umijs/bundler-webpackpackage.json
-            const utilsPkg = require('@umijs/bundler-utils/package.json');
-            const webpackPkg = require('@umijs/bundler-webpack/package.json');
-            version =
-              utilsPkg.devDependencies[part] ||
-              utilsPkg.dependencies[part] ||
-              webpackPkg.devDependencies[part] ||
-              webpackPkg.dependencies[part] ||
-              version;
-          }
+          const utilsPkg = require('@umijs/bundler-utils/package.json');
+          version =
+            utilsPkg.devDependencies[part] ||
+            utilsPkg.dependencies[part] ||
+            version;
+          versions.push(`${part}@${version}`);
+        });
+        parts.forEach((part) => {
+          versions.push(`${part}@${require(`${part}/package.json`).version}`);
+        });
+        bundlerParts.forEach((part) => {
+          let version = 'unknown';
+          const webpackPkg = require('@umijs/bundler-webpack/package.json');
+          version =
+            webpackPkg.devDependencies[part] ||
+            webpackPkg.dependencies[part] ||
+            version;
           versions.push(`${part}@${version}`);
         });
         versions.forEach((version) => {
